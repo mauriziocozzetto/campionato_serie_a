ddd54f29d484286e8cebcbd696e1a87acacc2b1e
Maurizio Cozzetto <maurizio.cozzetto@gmail.com>
2026-03-13 23:29:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -131,14 +131,10 @@ async def read_detail(match_id: int):
     return FileResponse('static/detail.html')
 
 
-@app.get("/matches/{match_id}/scorers/new")
-async def read_add_scorer(match_id: int):
-    return FileResponse('static/add_scorer.html')
-
-
 @app.get("/add-scorer")
 async def read_add_scorer():
     return FileResponse('static/add_scorer.html')
+
 
 # --- API ENDPOINTS ---
 
